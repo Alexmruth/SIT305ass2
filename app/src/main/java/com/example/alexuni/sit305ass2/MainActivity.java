74df6e987a1ca4424a69eaf8e9884d37fa450b9f
alexmruth <alexmruth@gmail.com>
2018-03-20 19:13:48 +1100

    created game activity

--- app/src/main/java/com/example/alexuni/sit305ass2/MainActivity.java
+++ app/src/main/java/com/example/alexuni/sit305ass2/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.alexuni.sit305ass2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button newGameBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newGameBtn = (Button) findViewById(R.id.newGameBtn);
+    }
+
+    public void onClick (View v) {
+        if (v.getId() == R.id.newGameBtn) {
+            Intent intent = new Intent (this, GameActivity.class);
+            startActivity(intent);
+        }
     }
 }
