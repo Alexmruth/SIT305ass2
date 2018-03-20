509da0bbfd5ae70bedbea17fe9d858785127cc73
alexmruth <alexmruth@gmail.com>
2018-03-20 21:24:07 +1100

    imported json library

--- app/src/main/java/com/example/alexuni/sit305ass2/GameActivity.java
+++ app/src/main/java/com/example/alexuni/sit305ass2/GameActivity.java
@@ -3,11 +3,17 @@ package com.example.alexuni.sit305ass2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.simple.JSONObject;
+
 public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        JSONObject obj = new JSONObject();
+
+
+
     }
 }
