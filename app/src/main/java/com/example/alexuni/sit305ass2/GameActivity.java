72b91a31d409aeaeb87607d39d348e69e117352b
aruth <alexmruth@gmail.cm>
2018-04-09 10:48:41 +1000

    finally basic working json, also began crating the UI for gameactivity

--- app/src/main/java/com/example/alexuni/sit305ass2/GameActivity.java
+++ app/src/main/java/com/example/alexuni/sit305ass2/GameActivity.java
@@ -1,13 +1,17 @@
 package com.example.alexuni.sit305ass2;
 
-import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
@@ -15,40 +19,59 @@ import java.io.UnsupportedEncodingException;
 public class GameActivity extends AppCompatActivity {
 
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 // https://www.javatpoint.com/android-json-parsing-tutorial
 // http://www.vogella.com/tutorials/AndroidJSON/article.html
-
-        String jsonString = loadJSONFromAsset();
         TextView textJSON = findViewById(R.id.textJSON);
-        textJSON.setText(jsonString);
-       try {
-            JSONObject json = new JSONObject(jsonString);
-            JSONObject test = json.getJSONObject("Characters");
-            String name = test.getString("name");
-            textJSON.setText(name);
+        String jsonString = loadJSON();
 
-        } catch (Exception e) {
+        //textJSON.setText(jsonString);
+        try {
+            getText();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
-       //getText();
 
     }
 
-    public String loadJSONFromAsset() {
+    public void main() {
+        TextView textJSON = findViewById(R.id.textJSON);
+        JSONParser parser = new JSONParser();
+        textJSON.setText("test");
+        try {
+            Object obj = parser.parse(new FileReader("test.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            String name = (String) jsonObject.get("Characters");
+            textJSON.setText(name);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createJSONFile() {
+
+
+    }
+    public String loadJSON() {
         String json = null;
        // String jsString;
         // JSONObject obj = new JSONObject(jsString);
         try {
-            InputStream is = getAssets().open("test.json");
-            int size = is.available();
+            InputStream file = getAssets().open("test.json");
+            int size = file.available();
             byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
+            file.read(buffer);
+            file.close();
             json = new String(buffer, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
@@ -60,23 +83,19 @@ public class GameActivity extends AppCompatActivity {
 
     }
 
-    /* public void getText() {
-        String name = null;
-        switch(textCase) {
-            case 1:
-                try {
-                    JSONObject json = new JSONObject(jsonString);
-                    JSONObject test = json.getJSONObject("Characters");
-                    name = test.getString("name");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                break;
-
+    public void getText() throws JSONException {
+        TextView textJSON = findViewById(R.id.textJSON);
+        loadJSON();
+        JSONObject obj = new JSONObject(loadJSON());
+        if(obj.getString("Name").equals("Name"))
+        {
+            String name = obj.getString("Name");
+            textJSON.setText(name);
+        } else {
+            textJSON.setText("banana");
         }
-        textJSON.setText(name);
-    } */
+
+    }
 
 }
 
