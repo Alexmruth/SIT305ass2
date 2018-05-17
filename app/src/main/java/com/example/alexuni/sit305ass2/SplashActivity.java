1e2848dffd04c4e59e8e039a996af31305815bc2
Alex <alexmruth@gmail.com>
2018-05-17 22:20:32 +1000

    improving code

--- app/src/main/java/com/example/alexuni/sit305ass2/SplashActivity.java
+++ app/src/main/java/com/example/alexuni/sit305ass2/SplashActivity.java
@@ -1,10 +1,10 @@
 package com.example.alexuni.sit305ass2;
 
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.os.Handler;
-        import android.support.annotation.Nullable;
-        import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 // The class SplashActivity is responsible for displaying the splash creen upon opening the app
 public class SplashActivity extends AppCompatActivity {
